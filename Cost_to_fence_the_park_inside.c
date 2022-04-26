2b059919616905447e77b1b464f963f015c4ba2f
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-04-26 21:12:22 +0530

    #include<stdio.h> int main() {     int l,b,w,c,ll,bl,f;     scanf("%d%d%d%d",&l,&b,&w,&c);     ll=l-2*w;     bl=b-2*w;     f=((l*b)-(ll*bl))*c;     if(ll<=0||bl<=0)     printf("Impossible");     else     printf("%d",f); }

---
+++ Cost_to_fence_the_park_inside.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,ll,bl,f;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    ll=l-2*w;
+    bl=b-2*w;
+    f=((l*b)-(ll*bl))*c;
+    if(ll<=0||bl<=0)
+    printf("Impossible");
+    else
+    printf("%d",f);
+}
