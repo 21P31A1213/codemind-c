90fcc013525bac19a63e7887c5dad5bceaeabdae
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-06-30 20:34:08 +0530

    #include<stdio.h> int main() {     int i,n,digit;     static int arr[10];     scanf("%d",&n);     while(n>0)     {         digit=n%10;         arr[digit]++;         n=n/10;     }     for(i=0;i<10;i++)     {         if(arr[i]>1)         {             printf("Not Unique Number");             break;         }     }     if(i==10)     {         printf("Unique Number");     }     return 0; }

--- Unique_number.c
+++ Unique_number.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,c,arr[100],i=0,f=0,p,j;
+    int i,n,digit;
+    static int arr[10];
     scanf("%d",&n);
-    while(n)
+    while(n>0)
     {
-        d=n%10;
-        arr[i++]=d;
+        digit=n%10;
+        arr[digit]++;
         n=n/10;
     }
-    for(p=0;p<i;p++)
+    for(i=0;i<10;i++)
     {
-        for(j=0;j<i;j++)
+        if(arr[i]>1)
         {
-            if(arr[p]==arr[j])
-            f++;
+            printf("Not Unique Number");
+            break;
         }
     }
-    if(f==i)
-    printf("Unique Number");
-    else
-    printf("Not Unique Number");
+    if(i==10)
+    {
+        printf("Unique Number");
+    }
+    return 0;
 }
