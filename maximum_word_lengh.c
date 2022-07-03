bd6b36406acc25517c8b6654ec66dbde1674ffa3
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-07-03 15:13:15 +0530

    #include<stdio.h> #include<string.h> int main() {     char a[100];     scanf("%[^ ]s",a);     int l=strlen(a),i,c=0,d=0,k=0;     for(i=0;i<l;i++)     {         if(a[i]!=' ')         {             c++;             d++;         }         else         {             d++;             if(k<c)             {                 k=c;             }             c=0;         }         if(d==l)         {             if(k<c)             {                 k=c;             }         }     }     if(c==l)     printf("%d",l);     else     printf("%d",k); }

---
+++ maximum_word_lengh.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    scanf("%[^
+]s",a);
+    int l=strlen(a),i,c=0,d=0,k=0;
+    for(i=0;i<l;i++)
+    {
+        if(a[i]!=' ')
+        {
+            c++;
+            d++;
+        }
+        else
+        {
+            d++;
+            if(k<c)
+            {
+                k=c;
+            }
+            c=0;
+        }
+        if(d==l)
+        {
+            if(k<c)
+            {
+                k=c;
+            }
+        }
+    }
+    if(c==l)
+    printf("%d",l);
+    else
+    printf("%d",k);
+}
