c115c67e42b3db0c0e6d7681fd2c6a3713d3757d
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-10-10 20:28:58 +0530

    #include<stdio.h> int main() {     int n,i,j;     float sum=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     int c=0,l=0;     for(i=0;i<n;i++)     {         c=0;         if(ar[i]==1)         c=1;         for(j=2;j<ar[i];j++)         {             if(ar[i]%j==0)             {                 c++;                 break;             }         }         if(c==0)         {             sum=sum+ar[i];             l++;         }     }     float z=sum,y=l,x;     x=z/l;     printf("%.2f",x); }

--- Average_of_primes.c
+++ Average_of_primes.c
@@ -1,35 +1,35 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int n,i,j;
     float sum=0;
     scanf("%d",&n);
     int ar[n];
     for(i=0;i<n;i++)
-{
-    scanf("%d",&ar[i]);
-}
-int c=0,l=0;
-for(i=0;i<n;i++)
-{
-    c=0;
-    if(ar[i]==1)
-    c=1;
-    for(j=2;j<ar[i];j++)
     {
-        if(ar[i]%j==0)
+        scanf("%d",&ar[i]);
+    }
+    int c=0,l=0;
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        if(ar[i]==1)
+        c=1;
+        for(j=2;j<ar[i];j++)
         {
-            c++;
-            break;
+            if(ar[i]%j==0)
+            {
+                c++;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            sum=sum+ar[i];
+            l++;
         }
     }
-    if(c==0)
-    {
-        sum=sum+ar[i];
-        l++;
-    }
-}
-float z=sum,y=l,x;
-x=z/l;
-printf("%.2f",x);
+    float z=sum,y=l,x;
+    x=z/l;
+    printf("%.2f",x);
 }
